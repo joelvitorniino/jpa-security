577378297c8b6c5c4459ea4bd806a3ff64c72806
joelvitorniino <joelprofessor13@gmail.com>
2022-11-16 10:10:49 -0300

    Spring boot project created

---
+++ src/main/java/com/joelvitorniino/jpasecurity/JpaSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.joelvitorniino.jpasecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joelvitorniino/jpasecurity/JpaSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joelvitorniino.jpasecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
